07f688e4dd516fcfc9e8463f9729a963ad7418fe
jhpertuz <jorgepertuz23@gmail.com>
2023-02-28 11:20:09 -0500

    cambios homework1

--- FT-M1-master/01-IntroToCS/homework/homework.js
+++ FT-M1-master/01-IntroToCS/homework/homework.js
@@ -1,8 +1,26 @@
 'use strict';
 
-function BinarioADecimal(num) {}
+function BinarioADecimal(num) {
+   var result=0;
+   var pos = num.length-1;
+   for(let i=0; i< num.length;i++){
+      result=result+num[i]*Math.pow(2,pos);
+      pos--;
+   }
+   return result;
 
-function DecimalABinario(num) {}
+}
+
+function DecimalABinario(num) {
+   var str =""
+   var number = num
+   while (number>0){
+      str=number %2 +str
+      number=math.floor(number/2)
+   }
+   console.log(str)
+   return str;
+}
 
 module.exports = {
    BinarioADecimal,
